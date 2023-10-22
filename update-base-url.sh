9b36a528c00f5c458dd3434a28ffa8692ea498f6
Tom Richardson <firecracker37@gmail.com>
2023-10-21 22:37:19 -0500

    Bash script rewritten to properly convert paths for GH Pages.

--- update-base-url.sh
+++ update-base-url.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 sed -i 's|<base href="/" />|<base href="/ConwayBlazorWASM/" />|' $1
+sed -i 's|<link href="/css|<link href="/ConwayBlazorWASM/css|' $1
+sed -i 's|<script src="/_content|<script src="/ConwayBlazorWASM/_content|' $1
+sed -i 's|<script src="/_framework|<script src="/ConwayBlazorWASM/_framework|' $1
