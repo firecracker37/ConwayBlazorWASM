0bc5d4da7dfd89e66637c203383269e76a818655
Tom Richardson <firecracker37@gmail.com>
2023-10-21 21:33:12 -0500

    Changed Base URL and how it is generated so that GH pages works.

--- ConwayClient/Program.cs
+++ ConwayClient/Program.cs
@@ -1,6 +1,7 @@
 using ConwayClient;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.JSInterop;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -8,4 +9,17 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-await builder.Build().RunAsync();
+var host = builder.Build();
+
+if (builder.HostEnvironment.IsDevelopment())
+{
+    var jsRuntime = host.Services.GetRequiredService<IJSRuntime>();
+    await jsRuntime.InvokeVoidAsync("setBaseUrl", "/");
+}
+else
+{
+    var jsRuntime = host.Services.GetRequiredService<IJSRuntime>();
+    await jsRuntime.InvokeVoidAsync("setBaseUrl", "/ConwayBlazorWASM/");
+}
+
+await host.RunAsync();
