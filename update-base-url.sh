cf756ffc21910e20b814f5c3c23d8fd249674356
Tom Richardson <firecracker37@gmail.com>
2023-10-21 22:46:59 -0500

    Changed resources in index.html so they will be defined with absolute URI instead of dynamic.

--- update-base-url.sh
+++ update-base-url.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sed -i 's|<base href="/" />|<base href="/ConwayBlazorWASM/" />|' $1
-sed -i 's|<link href="/css|<link href="/ConwayBlazorWASM/css|' $1
-sed -i 's|<script src="/_content|<script src="/ConwayBlazorWASM/_content|' $1
-sed -i 's|<script src="/_framework|<script src="/ConwayBlazorWASM/_framework|' $1
+sed -i 's|<link href="/css|<link href="https://firecracker37.github.io/ConwayBlazorWASM/css|' $1
+sed -i 's|<script src="/_content|<script src="https://firecracker37.github.io/ConwayBlazorWASM/_content|' $1
+sed -i 's|<script src="/_framework|<script src="https://firecracker37.github.io/ConwayBlazorWASM/_framework|' $1
