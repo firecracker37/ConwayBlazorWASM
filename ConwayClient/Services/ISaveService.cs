099576de7cd8363d75fad3c2e210ee9e393fbc4b
Tom Richardson <firecracker37@gmail.com>
2023-10-25 13:17:22 -0500

    Created ISaveService to manage save and load opperations
    Created Base64SaveService to implement ISaveService to handle save and load of games.

---
+++ ConwayClient/Services/ISaveService.cs
@@ -0,0 +1,10 @@
+﻿using ConwayClient.Models;
+
+namespace ConwayClient.Services
+{
+    public interface ISaveService
+    {
+        string Save(GameBoard gameBoard);
+        GameBoard Load(string savedData, int rows, int cols);
+    }
+}
